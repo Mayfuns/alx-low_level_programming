e35757612d629aacef31d498165a290d0857ab40
Mayfuns <mayfuns001@gmail.com>
2023-04-24 06:58:34 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A C program that prints a line to the standard error
- * Return 1 (Success)
+ *main - Entry point 
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
