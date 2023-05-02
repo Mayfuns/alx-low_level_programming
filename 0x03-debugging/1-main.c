36c70c4f3316c932b4c5aba49ffc900364bbc80d
Mayfuns <mayfuns001@gmail.com>
2023-05-02 10:03:10 -0700

    DEBUGGING

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,15 +10,11 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incomming :(\n");
-
 	i = 0;
-
 	/*while (i < 10)*/
 	/*{*/
 	/*	putchar(i);*/
 	/*}*/
-
 	printf("Infinite loop avoided! \\o/\n);
-
 	return (0);
 }
