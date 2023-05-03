85481c970c3715c65078fa8a3a4552367dcc1b4a
Mayfuns <mayfuns001@gmail.com>
2023-05-03 00:13:38 -0700

    5-rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,8 +4,6 @@
  * rev_string - Reverse a string
  * @s: The string to be modified
  * Return: void
- * declare tmp before first use
- * assign a value to tmp
  */
 void rev_string(char *s)
 {
@@ -17,6 +15,7 @@ void rev_string(char *s)
 	for (index = len - 1; index >= len / 2; index--)
 	{
 	char tmp;
+
 	tmp = s[index];
 	s[index] = s[len - index - 1];
 	s[len - index - 1] = tmp;
