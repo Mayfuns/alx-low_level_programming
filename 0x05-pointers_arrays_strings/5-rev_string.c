5d2e12f9a9a21d925350d612062215fa48519bba
Mayfuns <mayfuns001@gmail.com>
2023-05-03 00:09:33 -0700

    5-rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
-
 /**
  * rev_string - Reverse a string
  * @s: The string to be modified
  * Return: void
+ * declare tmp before first use
+ * assign a value to tmp
  */
-
 void rev_string(char *s)
 {
 	int len = 0, index = 0;
