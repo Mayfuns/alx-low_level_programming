6715093f348e6f8a04d097e93a1e9f573b22db8d
Mayfuns <mayfuns001@gmail.com>
2023-05-24 04:08:25 -0700

    dog.h

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -3,8 +3,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	return memcpy(dest, src, n);
+	return (memcpy(dest, src, n));
 }
