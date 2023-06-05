01343ddf4b6ed38299d38495cf33da7386c4b4cc
Mayfuns <mayfuns001@gmail.com>
2023-06-05 12:17:21 -0700

    Task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint- This prints all the elements of a linked list.
+ * @h: This is to print linked list of type listint_t to print
+ *
+ * Return: This is to return number of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+This is a project on C singly linked list.
+0x13-more_singly_linked_lists
