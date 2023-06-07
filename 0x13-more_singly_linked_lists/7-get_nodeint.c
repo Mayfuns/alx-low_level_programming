5089d134516f4dac041df6f70906d42d7fd973c2
Mayfuns <mayfuns001@gmail.com>
2023-06-07 12:01:09 -0700

    Task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,7 +4,10 @@
  * get_nodient_at_index - At a certain idex,returns the node in a linked list.
  * @head: This denotes index of the node to return
  *
- * Return: This is to return pointer to the node in search , or NULL
+ * @index: This is the index of the node to locate- indices start at 0
+ *
+ * Return: This is to return pointer to the node in search , or NULL.
+ * Otherwise - the located node.
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
@@ -15,7 +18,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		temp = temp->next;
 		i++;
-
 	}
 
 	return (temp ? temp : NULL);
