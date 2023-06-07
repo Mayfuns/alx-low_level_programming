7d476518e49abe87a1eff5c01e1cdea8cb043281
Mayfuns <mayfuns001@gmail.com>
2023-06-07 04:21:38 -0700

    Task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_nodient_at_index - At a certain idex,returns the node in a linked list.
+ * @head: This denotes index of the node to return
+ *
+ * Return: This is to return pointer to the node in search , or NULL
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *temp = head;
+
+	while (temp && i < index)
+	{
+		temp = temp->next;
+		i++;
+
+	}
+
+	return (temp ? temp : NULL);
+}
